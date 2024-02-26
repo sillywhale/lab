f25cf204000cc63607b6a83fab1beff9444e8ada
Ruslan <ezzezzrealtalk@gmail.com>
2024-02-26 16:54:30 +0500

    8 queens

--- lab6.cpp
+++ lab6.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
-#include <string>
+#define N 8
 
-int main()
-{
-  std::string str;
-  getline(std::cin, str);
-  std::cout << str;
+void printSolution(int board[N][N]) 
+{ 
+    for (int i = 0; i < N; i++) { 
+        for (int j = 0; j < N; j++) 
+            printf(" %d ", board[i][j]); 
+        printf("\n"); 
+    } 
+    } 
 
-  return 0;
-}
+bool isSafe(int board[N][N], int row, int col) 
+{ 
+    int i, j; 
+    for (i = 0; i < col; i++) 
+        if (board[row][i]) 
+            return false; 
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--) 
+        if (board[i][j]) 
+            return false; 
+    for (i = row, j = col; j >= 0 && i < N; i++, j--) 
+        if (board[i][j]) 
+            return false; 
+  
+    return true; 
+} 
+
+bool solve(int board[N][N], int col) 
+{ 
+    if (col >= N) 
+        return true; 
+    for (int i = 0; i < N; i++) { 
+        if (isSafe(board, i, col)) {
+            board[i][col] = 1;
+         
+            if (solve(board, col + 1)) 
+                return true; 
+        board[i][col] = 0; 
+        } 
+    }
+    return false; 
+} 
+
+int main() 
+{ 
+    int board[N][N] {}; 
+  
+    if (solve(board, 0) == false)
+        return 0;
+        
+    printSolution(board); 
+} 
