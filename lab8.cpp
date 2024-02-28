f44f80ced83f80a70af52712d2bacb2277cc85fc
Ruslan <ezzezzrealtalk@gmail.com>
2024-02-29 00:56:20 +0500

    str & struct

---
+++ lab8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+struct Book{
+  std::string Name;
+  std::string Author;
+  std::string published;
+  int pages;
+};
+
+int main()
+{
+  return 0;
+}
---
+++ labStr.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+ 
+using namespace std;
+
+void swap(string* a, size_t n){
+  for (int i = 0; i < n-1; i++){
+    for (int j = 0; j < n - 1 -i; j++){
+      if (a[j] > a[j+1]){
+        string temp = a[j];
+        a[j] = a[j+1];
+        a[j+1] = temp;
+      }
+    }
+  }
+}
+template <typename T>
+void print(T* arr, size_t N){
+  for (int i = 0; i < N; i++){
+    cout << arr[i] << ' ';
+  }
+}
+ 
+int main()
+{
+  string words;
+  getline(cin, words);
+  string temp;
+  stringstream ss;
+  ss << words;
+
+  int count = 0;
+  for (const auto& x : words){
+    if (x == ' ') count++; 
+  }
+  count++;
+
+  string* arr = new string[count];
+
+  int  i = 0;
+  while(ss >> temp)
+  {
+    if(isdigit(temp[0]))
+    {
+      arr[i] = temp;
+      i++;
+    }
+  }
+  swap(arr, i);
+  print(arr, i);
+
+  delete[] arr;
+  return 0;
+}
