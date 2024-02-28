452a6a17816e12a3ae551641b13ead4201493496
Ruslan <ezzezzrealtalk@gmail.com>
2024-02-28 22:53:42 +0000

    fixes

--- lab8.cpp
+++ lab8.cpp
@@ -2,13 +2,25 @@
 #include <string>
 
 struct Book{
-  std::string Name;
-  std::string Author;
+  std::string name;
+  std::string author;
   std::string published;
   int pages;
+  
+  void print(){
+    std::cout << name << ' ' << author << ' ' << published << ' ' << pages << '\n';
+  }
 };
 
 int main()
 {
+  Book Stalker;
+  Stalker.name = "Stalker";
+  Stalker.author = "Strugackie";
+  Stalker.published = "1978";
+  Stalker.pages = 221;
+
+  Stalker.print();
+  
   return 0;
 }
