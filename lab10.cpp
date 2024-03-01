6c99692a87a2532789fe4e5ed75a3f8499e4a42a
Ruslan <ezzezzrealtalk@gmail.com>
2024-03-01 11:05:07 +0500

    lab10

--- lab10.cpp
+++ lab10.cpp
@@ -2,30 +2,50 @@
 #include <string>
 using namespace std;
 
-#define n 4
+void print(string* arr,const int& n){
+  for (int i = 0;i < n; i++)
+    cout << arr[i] << '\n';
+}
+
+string* create(const int& n){
+  string* vec2 = new string[n];
+  int i = 0, j = 0;
+  for ( i; i < n; i++){
+      getline(cin, vec2[i]);
+  }
+
+  return vec2;
+  delete[] vec2;
+}
+
+template <typename T>
+void del(T* arr, int& n, const int& k ){
+  T* tmpArr = new T[n-1];
+  for (int i = 0; i < n; i++)
+    if ( i == k)
+      tmpArr[i] = arr[i+1];
+    else
+      tmpArr[i] = arr[i];
+  n--;
+  delete[] arr;
+  arr = tmpArr;
+  delete[] tmpArr;
+}
 
 int main()
 {
-  string** vec2 = new string*[n];
-  int i = 0, j = 0;
-  for ( i; i < n; i++){
-    vec2[i] = new string[n];
-    for ( j; j < n; j++){
-      getline(cin, vec2[i][j]);
-    }
-  }
+  int n = 4;
+  string* myList = create(n);
+  //print(myList, n);
 
   for (int i = 0; i < n; i++){
-    for (int j = 0; j < n; j++){
-      cout << vec2[i][j] << ' ';
+    if ( myList[i].front() == 'A' && myList[i].back() == 'a'){
+      del(myList, n, i);
     }
-    cout << endl;
   }
+  print(myList, n);
 
-  for (int k = 0; k < n; k++)
-    delete[] vec2[i];
-
-  delete[] vec2;
+  delete[] myList;
 
   return 0;
 }
