3e443706378d60f4b6a68fec9d7164313a6d4ff7
Ruslan <ezzezzrealtalk@gmail.com>
2024-03-02 23:36:05 +0500

    lab10 fnsh

--- lab10.cpp
+++ lab10.cpp
@@ -2,80 +2,41 @@
 #include <string>
 using namespace std;
 
-void print(string* arr,const int& n){
-  for (int i = 0;i < n; i++)
+template <typename T>
+void print(T* arr,const int& n){
+  for (int i = 0; i < n; i++)
     cout << arr[i] << '\n';
 }
 
 string* create(const int& n){
   string* vec2 = new string[n];
-  int i = 0, j = 0;
-  for ( i; i < n; i++){
+  for ( int i = 0; i < n; i++){
       getline(cin, vec2[i]);
   }
-
   return vec2;
   delete[] vec2;
 }
 
 template <typename T>
-T* del(T* arr, int& n, const int& k ){
-<<<<<<< HEAD
-  T* tmpArr = new T[n];
-  for (int i = 0; i < k; i++)
-    tmpArr[i] = arr[i];
-  for ( int  i = k; k < n-1; i++)
-    tmpArr[i] = arr[i+1];
-=======
-#ifdef false
-  T* tmpArr = new T[n-1];
-  for (int i = 0; i < n; i++)
-    if ( i == k)
-      tmpArr[i] = arr[i+1];
-    else
-      tmpArr[i] = arr[i];
-  n--;
->>>>>>> 8a5de902b05a70d3d65db233c510c27f563bef0b
-  delete[] arr;
-  n--;
-  arr = tmpArr;
-<<<<<<< HEAD
-=======
-  delete[] tmpArr;
-#endif
-  for (int i = k; i < n; i++)
-    arr[i-1] = arr[i];
-  n--;
->>>>>>> 8a5de902b05a70d3d65db233c510c27f563bef0b
-  return arr;
+void deletion(T* arr, int& n, int k){
+    for (int  i = k; i < n-1; i++)
+        arr[i] = arr[i+1];
+    n--;
 }
 
 int main()
 {
-  int n = 5;
+  int n {4};
   string* myList = create(n);
   int tmp = 0;
   for (int i = 0; i < n; i++){
-<<<<<<< HEAD
     if ( (myList[i].front() == 'A' || myList[i].front() == 'a') 
     && (myList[i].back() == 'a')){
-      tmp = i;
-=======
-    if ( myList[i].front() == 'A' && myList[i].back() == 'a'){
-      for (int j = i; j < n; j++)
-      myList[j-1] = myList[j];
-      n--;
->>>>>>> 8a5de902b05a70d3d65db233c510c27f563bef0b
+      deletion(myList, n, i);
     }
-
   }
-<<<<<<< HEAD
-  myList = del(myList, n, tmp);
-=======
   cout << endl;
->>>>>>> 8a5de902b05a70d3d65db233c510c27f563bef0b
   print(myList, n);
-
   delete[] myList;
 
   return 0;
