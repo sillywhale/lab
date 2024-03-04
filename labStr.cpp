1ff17df717243bd2acfc2264d9034a218b94440f
Ruslan <ezzezzrealtalk@gmail.com>
2024-03-04 21:00:21 +0000

    lab9, lab6

--- labStr.cpp
+++ labStr.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void swap(string* a, size_t n){
   for (int i = 0; i < n-1; i++){
     for (int j = 0; j < n - 1 -i; j++){
-      if (size(a[j]) > size(a[j+1])){
+      if ( a[j].size() > a[j+1].size() ){
         string temp = a[j];
         a[j] = a[j+1];
         a[j+1] = temp;
